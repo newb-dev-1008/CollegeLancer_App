a1c84151cd2da78f202a6a5a00cac3836bbf0823
Yash Umale <yashumale223@gmail.com>
2020-06-05 19:11:44 +0530

    Setting up ProfessorMainActivity

---
+++ app/src/main/java/com/college/freelancestartup/ProfessorMainActivity.java
@@ -0,0 +1,51 @@
+package com.college.freelancestartup;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ProfessorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_home);
+
+        BottomNavigationView studentNavBottom = findViewById(R.id.studentHomeNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.studentHomeFragContainer,
+                new StudentHomeFragment()).commit();
+
+        studentNavBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.profHome:
+                        selectedFragment = new ProfessorHomeFragment();
+                        break;
+                    case R.id.profAddProj:
+                        selectedFragment = new ProfessorAddProjFragment();
+                        break;
+                    case R.id.studentOrgs:
+                        selectedFragment = new ProfessorOrgsFragment();
+                        break;
+                    case R.id.profStudent:
+                        selectedFragment = new ProfStudentFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.studentHomeFragContainer,
+                        selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
