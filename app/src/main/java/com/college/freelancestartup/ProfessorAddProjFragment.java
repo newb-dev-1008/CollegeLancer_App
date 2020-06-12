ac46a543f3f6fc24a5b332a2e71dc00c6dfe00b5
Yash Umale <yashumale223@gmail.com>
2020-06-12 15:54:48 +0530

    ProfessorAddProjFragment done with the basic stuff

--- app/src/main/java/com/college/freelancestartup/ProfessorAddProjFragment.java
+++ app/src/main/java/com/college/freelancestartup/ProfessorAddProjFragment.java
@@ -4,16 +4,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfessorAddProjFragment extends Fragment {
+
+    private EditText profProjectTitle, profProjectDescription, profProjectPriority;
+    private MaterialButton profCreateProjectButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        profProjectTitle = getView().findViewById(R.id.profProjectTitleET);
+        profProjectDescription = getView().findViewById(R.id.profProjectDescriptionET);
+        profProjectPriority = getView().findViewById(R.id.profProjectPriorityET);
+        profCreateProjectButton = getView().findViewById(R.id.profCreateProjectButton);
+
+        profCreateProjectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveProfCreatedProject();
+            }
+        });
         return inflater.inflate(R.layout.prof_addproj_frag, container, false);
     }
+
+    private void saveProfCreatedProject(){
+        String projectTitle = profProjectTitle.getText().toString();
+        String projectDescription = profProjectDescription.getText().toString();
+        int projectPriority = Integer.parseInt(profProjectPriority.getText().toString());
+
+        if (projectTitle.trim().isEmpty() || projectDescription.trim().isEmpty()){
+            Toast.makeText(getContext(),"Please enter a valid project title and description.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        CollectionReference addProjectRef = FirebaseFirestore.getInstance()
+                .collection("Projects");
+        addProjectRef.add(new StudentProjectsListClass(projectTitle, projectDescription, projectPriority));
+        Toast.makeText(getContext(),"Project added.", Toast.LENGTH_SHORT).show();
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.student_fragment_container, new ProfessorHomeFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
 }
