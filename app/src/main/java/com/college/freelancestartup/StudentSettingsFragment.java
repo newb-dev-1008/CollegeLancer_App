1878e6e0e23cb858fec56261bfc98acfa04c9e68
Yash Umale <yashumale223@gmail.com>
2020-06-17 15:49:55 +0530

    StudentSettingsFragment basics done

--- app/src/main/java/com/college/freelancestartup/StudentSettingsFragment.java
+++ app/src/main/java/com/college/freelancestartup/StudentSettingsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +13,21 @@ import androidx.fragment.app.Fragment;
 
 public class StudentSettingsFragment extends Fragment {
 
+    String[] studentSettingsArray;
+    ListView studentSettingsListView;
+    View root;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.student_settings_frag, container, false);
+
+        studentSettingsArray = getResources().getStringArray(R.array.studentSettingsArray);
+        root = inflater.inflate(R.layout.student_settings_frag, container, false);
+        studentSettingsListView = root.findViewById(R.id.student_settings_listview);
+
+        ArrayAdapter studentSettingsAdapter = new ArrayAdapter<String>(getActivity(), R.layout.student_settings_frag, studentSettingsArray);
+        studentSettingsListView.setAdapter(studentSettingsAdapter);
+
+        return root;
     }
 }
