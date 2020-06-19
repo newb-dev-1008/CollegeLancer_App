1e4c19842786a656ec90e45004db41d7d4df6a7b
Yash Umale <yashumale223@gmail.com>
2020-06-19 20:26:33 +0530

    ProfessorSettingsFragment ListView Adapter added

--- app/src/main/java/com/college/freelancestartup/ProfessorSettingsFragment.java
+++ app/src/main/java/com/college/freelancestartup/ProfessorSettingsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +13,19 @@ import androidx.fragment.app.Fragment;
 
 public class ProfessorSettingsFragment extends Fragment {
 
+    String[] profSettingsArray;
+    ListView profSettingsListView;
+    View root;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.prof_settings_frag, container, false);
+        profSettingsArray = getResources().getStringArray(R.array.profSettingsArray);
+        root = inflater.inflate(R.layout.prof_settings_frag, container, false);
+        profSettingsListView = root.findViewById(R.id.prof_settings_listview);
+
+        ArrayAdapter<String> studentSettingsAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, profSettingsArray);
+        profSettingsListView.setAdapter(studentSettingsAdapter);
+
+        return root;
     }
 }
