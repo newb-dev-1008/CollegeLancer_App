5a507b8c50b3d3ef2ddaa79bc4fa41ee8f974fb2
Yash Umale <yashumale223@gmail.com>
2020-06-22 15:52:40 +0530

    StudentSettingsFragment status updates (6) [DialogAlert stuff]

--- app/src/main/java/com/college/freelancestartup/StudentSettingsFragment.java
+++ app/src/main/java/com/college/freelancestartup/StudentSettingsFragment.java
@@ -1,5 +1,6 @@
 package com.college.freelancestartup;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +11,10 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -55,6 +58,16 @@ public class StudentSettingsFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 switch (i) {
+                    case 0:
+                        String[] status = {"Available for research (with colleagues)", "Available for research (with colleagues)", "Looking for students", "Busy, but can provide projects", "Unavailable for a while"};
+                        AlertDialog statusSetting = new MaterialAlertDialogBuilder(getContext())
+                                .setTitle("Set your current status")
+                                .setMessage("Please note that your status determines your availability for providing and receiving projects.")
+                                .setSingleChoiceItems(status, checkedStatus, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int statusNo) {
+                                    }
+                                })
                 }
             }
         });
