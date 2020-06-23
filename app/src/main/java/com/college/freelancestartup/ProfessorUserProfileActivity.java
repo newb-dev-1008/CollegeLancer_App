dd78c2cbefbb0ec4a1a4c6a31c1408057a7c0127
Yash Umale <yashumale223@gmail.com>
2020-06-23 15:35:07 +0530

    ProfessorUserProfileActivity update (1)

--- app/src/main/java/com/college/freelancestartup/ProfessorUserProfileActivity.java
+++ app/src/main/java/com/college/freelancestartup/ProfessorUserProfileActivity.java
@@ -1,4 +1,14 @@
 package com.college.freelancestartup;
 
-class ProfessorUserProfileActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class ProfessorUserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
