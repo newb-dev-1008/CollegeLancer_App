a299d62111c0c1508b7d667536d06eb046c58f81
Yash Umale <yashumale223@gmail.com>
2020-07-02 13:38:05 +0530

    Student User Profile XML (Apply Changes) added

--- app/src/main/java/com/college/freelancestartup/StudentUserProfile.java
+++ app/src/main/java/com/college/freelancestartup/StudentUserProfile.java
@@ -1,4 +1,17 @@
 package com.college.freelancestartup;
 
-class StudentUserProfile {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class StudentUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_user_profile_settings);
+    }
+
+
 }
