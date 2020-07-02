7cdfe0d09ef50fb39fa5ad93456867cbfb1407fe
Yash Umale <yashumale223@gmail.com>
2020-07-02 14:10:07 +0530

    Student User Profile XML and StudentUserProfile

--- app/src/main/java/com/college/freelancestartup/StudentUserProfile.java
+++ app/src/main/java/com/college/freelancestartup/StudentUserProfile.java
@@ -1,16 +1,33 @@
 package com.college.freelancestartup;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 class StudentUserProfile extends AppCompatActivity {
+
+    private EditText phoneNumberET, departmentET, semesterET, emailET, DOBET, universityET, bioET;
+    private MaterialButton applyChanges, cancelChanges;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_user_profile_settings);
+
+        phoneNumberET = findViewById(R.id.student_profilePhNo);
+        departmentET = findViewById(R.id.student_profileDepartment);
+        semesterET = findViewById(R.id.student_profileSemester);
+        emailET = findViewById(R.id.student_profileEmail);
+        DOBET = findViewById(R.id.student_profileDOB);
+        universityET = findViewById(R.id.student_profileUniversity);
+        bioET = findViewById(R.id.student_profileBio);
+
+        applyChanges = findViewById(R.id.student_apply_changes);
+        cancelChanges = findViewById(R.id.student_cancel_changes);
     }
 
 
