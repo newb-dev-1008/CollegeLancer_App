7d114867f7ec3e98505e82738acb2cbd0c0c7d72
Yash Umale <yashumale223@gmail.com>
2020-07-06 12:06:17 +0530

    Added OTP dependency to app level build.gradle

--- app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
+++ app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
@@ -1,4 +1,14 @@
 package com.college.freelancestartup;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 class UpdatePhoneNumberOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
