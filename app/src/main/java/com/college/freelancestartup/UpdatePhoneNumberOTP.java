2028bdd4bc6022d2eff69e6320178e47aa2542a8
Yash Umale <yashumale223@gmail.com>
2020-07-06 12:31:35 +0530

    Activity initializations

--- app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
+++ app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
@@ -1,9 +1,15 @@
 package com.college.freelancestartup;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+import in.aabhasjindal.otptextview.OtpTextView;
 
 class UpdatePhoneNumberOTP extends AppCompatActivity {
 
@@ -11,5 +17,10 @@ class UpdatePhoneNumberOTP extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phone_number_verification);
+
+        OtpTextView phoneNumberOTP;
+        TextView phoneNumberOTPTextView;
+        MaterialButton phoneVerificationOTPButton;
+        ProgressBar phoneVerificationProgressBar;
     }
 }
