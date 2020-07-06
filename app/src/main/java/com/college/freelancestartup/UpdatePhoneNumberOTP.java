0bff545f9206ed1a5bbf2e2f491c3b5e021bf365
Yash Umale <yashumale223@gmail.com>
2020-07-06 12:29:09 +0530

    Linked Activity to Layout XML

--- app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
+++ app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
@@ -10,5 +10,6 @@ class UpdatePhoneNumberOTP extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.phone_number_verification);
     }
 }
