e9f72fb8e0745c62b1b2d59a3e3a4a8ea34b5ff2
Yash Umale <yashumale223@gmail.com>
2020-07-06 13:46:29 +0530

    Adding firebase phone authentication

--- app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
+++ app/src/main/java/com/college/freelancestartup/UpdatePhoneNumberOTP.java
@@ -11,23 +11,33 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 import in.aabhasjindal.otptextview.OTPListener;
 import in.aabhasjindal.otptextview.OtpTextView;
 
 class UpdatePhoneNumberOTP extends AppCompatActivity {
 
+    String phoneNumber;
+    Integer realPhoneNo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phone_number_verification);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
         OtpTextView phoneNumberOTP = findViewById(R.id.phoneVerificationOTPView);
         TextView phoneNumberOTPTextView = findViewById(R.id.phoneVerificationTV);
         MaterialButton phoneVerificationOTPButton = findViewById(R.id.phoneVerificationOTPButton);
         ProgressBar phoneVerificationProgressBar;
 
-        String phoneNumber = getIntent().getExtras().getString("phoneNo");
+        phoneNumber = getIntent().getExtras().getString("phoneNo");
+        realPhoneNo = Integer.parseInt("+91" + phoneNumber);
         String displayPhoneNo = "+91 - " + phoneNumber;
         String textViewDisplay = "Enter the OTP sent to your phone number:\n" + displayPhoneNo;
 
@@ -48,8 +58,13 @@ class UpdatePhoneNumberOTP extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onOTPComplete(String OTP) {
-        // fired when user has entered the OTP fully.
+    private void verifyPhoneNumber(){
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                realPhoneNo,
+                60,
+                TimeUnit.SECONDS;
+                this,
+                mCallbacks);
+        );
     }
 }
