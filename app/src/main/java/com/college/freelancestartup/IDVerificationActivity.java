379fa74002d92b6bd3be20622487e570d06c0f1c
Yash Umale <yashumale223@gmail.com>
2020-07-10 11:06:14 +0530

    CaptureImageActivity update (14) [Test XML and class for text recognition]

--- app/src/main/java/com/college/freelancestartup/IDVerificationActivity.java
+++ app/src/main/java/com/college/freelancestartup/IDVerificationActivity.java
@@ -2,6 +2,7 @@ package com.college.freelancestartup;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +10,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 class IDVerificationActivity extends AppCompatActivity {
 
+    TextView testTextview;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.test_text_recognition);
+        testTextview = findViewById(R.id.text_test);
+
+        String testingText = getIntent().getExtras().getString("test_text");
+        testTextview.setText(testingText);
     }
 }
