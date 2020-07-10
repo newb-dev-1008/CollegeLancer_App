532955a68f4768a2f1ca3a8918e16750b5afbb8d
Yash Umale <yashumale223@gmail.com>
2020-07-10 10:52:09 +0530

    CaptureImageActivity update (10) [Test XML and class for text recognition]

--- app/src/main/java/com/college/freelancestartup/IDVerificationActivity.java
+++ app/src/main/java/com/college/freelancestartup/IDVerificationActivity.java
@@ -6,13 +6,13 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.mlkit.vision.common.InputImage;
 
 class IDVerificationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 }
