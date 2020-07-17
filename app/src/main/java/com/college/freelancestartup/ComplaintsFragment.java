a5e35410114743d02722d530bfe2dfaaa89a5bc9
Yash Umale <yashumale223@gmail.com>
2020-07-17 14:49:09 +0530

    Complaints XML updates (12)

--- app/src/main/java/com/college/freelancestartup/ComplaintsFragment.java
+++ app/src/main/java/com/college/freelancestartup/ComplaintsFragment.java
@@ -1,4 +1,29 @@
 package com.college.freelancestartup;
 
-class ComplaintsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+
+public class ComplaintsFragment extends Fragment {
+
+    private View root;
+    private MaterialButton lodgeComplaintButton, writeComplaintEmailButton;
+    private EditText complaintET;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.help_complaints_frag, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+        lodgeComplaintButton = root.findViewById(R.id.lodgeComplaintButton);
+    }
 }
