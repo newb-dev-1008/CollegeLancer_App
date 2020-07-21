3c777cd03fe5777a7dd7931fd0a8ee57efc6293e
Yash Umale <yashumale223@gmail.com>
2020-07-21 16:53:11 +0530

    Adjusting RegComplaintsFragment according to ArrayList of complaints (1)

--- app/src/main/java/com/college/freelancestartup/RegComplaintsFragment.java
+++ app/src/main/java/com/college/freelancestartup/RegComplaintsFragment.java
@@ -9,7 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -17,12 +20,24 @@ public class RegComplaintsFragment extends Fragment {
 
     private View root;
     private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.reg_complaints_frag, container, false);
+        db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        db.collection("Users").document("User " + firebaseAuth.getCurrentUser().getEmail())
+                .collection("Complaints").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if (queryDocumentSnapshots.size() > 0){
+
+                }
+            }
+        })
         ArrayList<RegisteredComplaints> registeredComplaints = new ArrayList<>();
         return root;
     }
