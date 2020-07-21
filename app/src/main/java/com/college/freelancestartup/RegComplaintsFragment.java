714b40c0194930339b43bc6b94e14d9af515848f
Yash Umale <yashumale223@gmail.com>
2020-07-21 16:44:21 +0530

    Adjusting RegComplaintsFragment according to ArrayList of complaints

--- app/src/main/java/com/college/freelancestartup/RegComplaintsFragment.java
+++ app/src/main/java/com/college/freelancestartup/RegComplaintsFragment.java
@@ -9,14 +9,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
 public class RegComplaintsFragment extends Fragment {
 
     private View root;
+    private FirebaseFirestore db;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.reg_complaints_frag, container, false);
+
+        ArrayList<RegisteredComplaints> registeredComplaints = new ArrayList<>();
         return root;
     }
 }
