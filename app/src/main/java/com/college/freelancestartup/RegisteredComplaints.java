13294517abd3930927bc619dac96557b49cda3f3
Yash Umale <yashumale223@gmail.com>
2020-07-21 16:39:21 +0530

    Setting up Java class for initializing RecyclerView objects

--- app/src/main/java/com/college/freelancestartup/RegisteredComplaints.java
+++ app/src/main/java/com/college/freelancestartup/RegisteredComplaints.java
@@ -1,6 +1,25 @@
 package com.college.freelancestartup;
 
 class RegisteredComplaints {
+    private String complaintTitle;
+    private String complaintDate;
+    private String complaintStatus;
 
+    public RegisteredComplaints(String complaintTitle, String complaintDate, String complaintStatus) {
+        this.complaintTitle = complaintTitle;
+        this.complaintDate = complaintDate;
+        this.complaintStatus = complaintStatus;
+    }
 
+    public String getComplaintTitle() {
+        return complaintTitle;
+    }
+
+    public String getComplaintDate() {
+        return complaintDate;
+    }
+
+    public String getComplaintStatus() {
+        return complaintStatus;
+    }
 }
