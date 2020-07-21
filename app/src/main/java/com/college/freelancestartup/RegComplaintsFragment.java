5b861ec69919fefc2d2b374b066a8a5df67f62c3
Yash Umale <yashumale223@gmail.com>
2020-07-21 18:04:25 +0530

    Adjusting RegComplaintsFragment according to ArrayList of complaints (3)

--- app/src/main/java/com/college/freelancestartup/RegComplaintsFragment.java
+++ app/src/main/java/com/college/freelancestartup/RegComplaintsFragment.java
@@ -5,13 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -41,11 +44,19 @@ public class RegComplaintsFragment extends Fragment {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 if (queryDocumentSnapshots.size() > 0){
-
+                    emptyRegComplaintsTV.setVisibility(View.GONE);
+                    regComplaintsRecyclerView.setVisibility(View.VISIBLE);
+                    ArrayList<RegisteredComplaints> registeredComplaints = new ArrayList<>();
+                } else {
+                    emptyRegComplaintsTV.setVisibility(View.VISIBLE);
                 }
             }
-        })
-        ArrayList<RegisteredComplaints> registeredComplaints = new ArrayList<>();
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return root;
     }
 }
