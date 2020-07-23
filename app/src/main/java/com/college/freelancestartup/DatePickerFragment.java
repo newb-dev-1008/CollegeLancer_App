b6ddb4f382382df8731b940998a223e3b6e716fa
Yash Umale <yashumale223@gmail.com>
2020-07-23 19:51:49 +0530

    Changing DatePicker property in StudentUserProfile (2)

--- app/src/main/java/com/college/freelancestartup/DatePickerFragment.java
+++ app/src/main/java/com/college/freelancestartup/DatePickerFragment.java
@@ -13,16 +13,29 @@ import java.util.Calendar;
 
 public class DatePickerFragment extends DialogFragment {
 
-    private int pickedYear, pickedDay, pickedMonth;
+    private int pickedYear = 0, pickedDay = 0, pickedMonth = 0, year, month, day;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        if (pickedYear == 0) {
+            year = calendar.get(Calendar.YEAR);
+            month = calendar.get(Calendar.MONTH);
+            day = calendar.get(Calendar.DAY_OF_MONTH);
+        } else {
+            year = pickedYear;
+            month = pickedMonth;
+            day = pickedDay;
+        }
 
         return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+    }
+
+    public void getPickedDate(int pickedDay1, int pickedMonth1, int pickedYear1){
+        pickedDay = pickedDay1;
+        pickedMonth = pickedMonth1;
+        pickedYear = pickedYear1;
     }
 }
