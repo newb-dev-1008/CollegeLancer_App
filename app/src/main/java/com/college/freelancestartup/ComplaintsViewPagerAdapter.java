6fb89a16b363d28f9df3f56f6e96c5b1f380e376
Yash Umale <yashumale223@gmail.com>
2020-07-23 20:04:08 +0530

    Fixing TabLayout titles bug

--- app/src/main/java/com/college/freelancestartup/ComplaintsViewPagerAdapter.java
+++ app/src/main/java/com/college/freelancestartup/ComplaintsViewPagerAdapter.java
@@ -1,14 +1,23 @@
 package com.college.freelancestartup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class ComplaintsViewPagerAdapter extends FragmentPagerAdapter {
 
+    private String[] tabTitles = new String[]{"New Complaints", "Registered Complaints"};
+
     public ComplaintsViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
         super(fm, behavior);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
     }
 
     @Override
