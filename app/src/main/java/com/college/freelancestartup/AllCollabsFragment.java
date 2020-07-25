68483c323824b2e9dcba0f59ecd84e95a1a1c57a
Yash Umale <yashumale223@gmail.com>
2020-07-25 17:39:53 +0530

    Adding activities for all collab fragments (2)

--- app/src/main/java/com/college/freelancestartup/AllCollabsFragment.java
+++ app/src/main/java/com/college/freelancestartup/AllCollabsFragment.java
@@ -4,19 +4,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 class AllCollabsFragment extends Fragment {
 
     private View root;
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
+    private RecyclerView collab1RecyclerView;
+    private TextView swipeDownRefreshTV, emptyTV;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.student_find_collabs_frag, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        collab1RecyclerView = root.findViewById(R.id.find_collabs_recyclerView);
+        swipeDownRefreshTV = root.findViewById(R.id.swipeRefreshTVCollab1);
+        emptyTV = root.findViewById(R.id.find_collab1_emptyTV);
 
         return root;
     }
