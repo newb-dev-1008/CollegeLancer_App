8ba02a02114c169d08302b7c0204c6056af1d375
Yash Umale <yashumale223@gmail.com>
2020-07-25 17:33:36 +0530

    Adding activities for all collab fragments (1)

--- app/src/main/java/com/college/freelancestartup/AllCollabsFragment.java
+++ app/src/main/java/com/college/freelancestartup/AllCollabsFragment.java
@@ -1,4 +1,25 @@
 package com.college.freelancestartup;
 
-class AllCollabsFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+class AllCollabsFragment extends Fragment {
+
+    private View root;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.student_find_collabs_frag, container, false);
+
+        return root;
+    }
 }
+
+
