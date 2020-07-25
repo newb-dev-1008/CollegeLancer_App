818c6a27f3c899e4806dae35c2a08cae658c2519
Yash Umale <yashumale223@gmail.com>
2020-07-25 18:32:13 +0530

    First Collab Tab setting up (4)

--- app/src/main/java/com/college/freelancestartup/AllColabsOne.java
+++ app/src/main/java/com/college/freelancestartup/AllColabsOne.java
@@ -1,4 +1,43 @@
 package com.college.freelancestartup;
 
 class AllColabsOne {
+    private String posterTitle;
+    private String projectTitle;
+    private String projectDesc;
+    private String collabDate;
+    private String skills;
+    private String openFor;
+
+    public AllColabsOne(String posterTitle, String projectTitle, String projectDesc, String collabDate, String skills, String openFor) {
+        this.posterTitle = posterTitle;
+        this.projectTitle = projectTitle;
+        this.projectDesc = projectDesc;
+        this.collabDate = collabDate;
+        this.skills = skills;
+        this.openFor = openFor;
+    }
+
+    public String getPosterTitle() {
+        return posterTitle;
+    }
+
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+
+    public String getProjectDesc() {
+        return projectDesc;
+    }
+
+    public String getCollabDate() {
+        return collabDate;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getOpenFor() {
+        return openFor;
+    }
 }
