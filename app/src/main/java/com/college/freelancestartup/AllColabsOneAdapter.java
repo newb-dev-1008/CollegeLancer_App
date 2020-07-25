44982b7226b05f47b6a3d982271b6cc1dce3cae1
Yash Umale <yashumale223@gmail.com>
2020-07-25 18:42:18 +0530

    First Collab Tab setting up (5)

---
+++ app/src/main/java/com/college/freelancestartup/AllColabsOneAdapter.java
@@ -0,0 +1,37 @@
+package com.college.freelancestartup;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+class AllColabsOneAdapter extends RecyclerView.Adapter<AllColabsOneAdapter.AllColabsOneViewHolder> {
+
+    private ArrayList<AllColabsOne> AllColabsList;
+
+    public static class AllColabsOneViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView posterName1, projectTitle1, postedDate1, projectDesc1, openFor1, skills1;
+
+        public AllColabsOneViewHolder(@NonNull View itemView) {
+            posterName1 = itemView.findViewById(R.id.collab1_name);
+            projectTitle1 = itemView.findViewById(R.id.collab1_projectTitle);
+            postedDate1 = itemView.findViewById(R.id.collab1_date);
+            projectDesc1 = itemView.findViewById(R.id.collab1_projectDesc);
+            openFor1 = itemView.findViewById(R.id.collab1_openFor);
+            skills1 = itemView.findViewById(R.id.collab1_skills);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }
+    }
+
+
+}
