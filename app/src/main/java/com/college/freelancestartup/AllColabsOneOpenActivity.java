00ef39533047f127db657802fdfe6b777b53c9f2
Yash Umale <yashumale223@gmail.com>
2020-07-26 17:27:53 +0530

    Second Collab Fragment setting up (6)

---
+++ app/src/main/java/com/college/freelancestartup/AllColabsOneOpenActivity.java
@@ -0,0 +1,14 @@
+package com.college.freelancestartup;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class AllColabsOneOpenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
