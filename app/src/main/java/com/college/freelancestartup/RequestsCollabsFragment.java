714fdcacfe3bfd9d70c0b55bcf4e73c68cccf2ff
Yash Umale <yashumale223@gmail.com>
2020-07-26 15:11:18 +0530

    Second Collab Fragment setting up (2)

--- app/src/main/java/com/college/freelancestartup/RequestsCollabsFragment.java
+++ app/src/main/java/com/college/freelancestartup/RequestsCollabsFragment.java
@@ -1,8 +1,13 @@
 package com.college.freelancestartup;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
@@ -19,5 +24,18 @@ class RequestsCollabsFragment extends Fragment {
     private TextView swipeDownRefreshTV, emptyTV;
     private SwipeRefreshLayout swipeRefreshLayout;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.student_collab_requests_frag, container, false);
 
+        db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        collab2RecyclerView = root.findViewById(R.id.request_collabs_recyclerView);
+        swipeDownRefreshTV = root.findViewById(R.id.swipeRefreshTVCollab2);
+
+
+        return root;
+    }
 }
