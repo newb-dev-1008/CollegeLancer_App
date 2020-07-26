b5a0f0b6364b368543e181eb633203d2ff88649c
Yash Umale <yashumale223@gmail.com>
2020-07-26 17:23:47 +0530

    Second Collab Fragment setting up (5)

--- app/src/main/java/com/college/freelancestartup/RequestCollabTwo.java
+++ app/src/main/java/com/college/freelancestartup/RequestCollabTwo.java
@@ -7,4 +7,37 @@ class RequestCollabTwo {
     private String collabDate;
     private String skills;
     private String openFor;
+
+    public RequestCollabTwo(String posterTitle, String projectTitle, String projectDesc, String collabDate, String skills, String openFor) {
+        this.posterTitle = posterTitle;
+        this.projectTitle = projectTitle;
+        this.projectDesc = projectDesc;
+        this.collabDate = collabDate;
+        this.skills = skills;
+        this.openFor = openFor;
+    }
+
+    public String getPosterTitle() {
+        return posterTitle;
+    }
+
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+
+    public String getProjectDesc() {
+        return projectDesc;
+    }
+
+    public String getCollabDate() {
+        return collabDate;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getOpenFor() {
+        return openFor;
+    }
 }
