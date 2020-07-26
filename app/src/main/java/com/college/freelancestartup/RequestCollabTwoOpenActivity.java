11f5cbfe452fa4fa9c0c8e426187a60cc5181aac
Yash Umale <yashumale223@gmail.com>
2020-07-26 18:30:52 +0530

    Second Collab Fragment setting up (14)

--- app/src/main/java/com/college/freelancestartup/RequestCollabTwoOpenActivity.java
+++ app/src/main/java/com/college/freelancestartup/RequestCollabTwoOpenActivity.java
@@ -1,6 +1,32 @@
 package com.college.freelancestartup;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-class RequestCollabTwoOpenActivity extends AppCompatActivity {
+public class RequestCollabTwoOpenActivity extends AppCompatActivity {
+
+    private TextView posterName1, projectTitle1, postedDate1, projectDesc1, openFor1, skills1;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.find_collab1_opencard);
+
+        posterName1 = findViewById(R.id.collab1_name);
+        projectTitle1 = findViewById(R.id.collab1_projectTitle);
+        postedDate1 = findViewById(R.id.collab1_date);
+        projectDesc1 = findViewById(R.id.collab1_projectDesc);
+        openFor1 = findViewById(R.id.collab1_openFor);
+        skills1 = findViewById(R.id.collab1_skills);
+
+        posterName1.setText(getIntent().getExtras().get("posterName1").toString());
+        projectTitle1.setText(getIntent().getExtras().get("projectTitle1").toString());
+        postedDate1.setText(getIntent().getExtras().get("postedDate1").toString());
+        projectDesc1.setText(getIntent().getExtras().get("projectDesc1").toString());
+        openFor1.setText(getIntent().getExtras().get("openFor1").toString());
+        skills1.setText(getIntent().getExtras().get("skills1").toString());
+    }
 }
