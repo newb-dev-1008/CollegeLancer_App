f4b734892b4b709edbe5037e73f5f9ec4b0722ac
Yash Umale <yashumale223@gmail.com>
2020-07-27 17:01:51 +0530

    Fifth collab cardview XML set up (3)

---
+++ app/src/main/java/com/college/freelancestartup/AvailableCollabsFragment.java
@@ -0,0 +1,30 @@
+package com.college.freelancestartup;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+class AvailableCollabsFragment extends Fragment {
+
+    private View root;
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.student_collab_available_frag, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        return root;
+    }
+}
