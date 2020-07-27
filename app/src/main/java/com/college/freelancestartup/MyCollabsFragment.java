c5609492dd327f8cf420fe1ea8a71e2abefc318d
Yash Umale <yashumale223@gmail.com>
2020-07-27 15:45:50 +0530

    Fourth collab ViewPager setting up (1)

--- app/src/main/java/com/college/freelancestartup/MyCollabsFragment.java
+++ app/src/main/java/com/college/freelancestartup/MyCollabsFragment.java
@@ -9,12 +9,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 class MyCollabsFragment extends Fragment {
 
@@ -24,6 +30,9 @@ class MyCollabsFragment extends Fragment {
     private TextView swipeDownRefreshTV, emptyTV;
     private FirebaseFirestore db;
     private FirebaseAuth firebaseAuth;
+
+    private RecyclerView.LayoutManager myCollabs4LayoutManager;
+    private RecyclerView.Adapter myCollabs4Adapter;
 
     @Nullable
     @Override
@@ -48,6 +57,35 @@ class MyCollabsFragment extends Fragment {
     }
 
     private void showCollab4(){
+        db.collection("Users").document("User " + firebaseAuth.getCurrentUser().getEmail())
+                .collection("MyCollabs").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if (queryDocumentSnapshots.size() > 0) {
+                    emptyTV.setVisibility(View.GONE);
+                    ArrayList<MyCollabsFour> myCollabs = new ArrayList<>();
+                    for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        String posterTitle = documentSnapshot.get("posterTitle").toString();
+                        String projectTitle = documentSnapshot.get("projectTitle").toString();
+                        String postedDate = documentSnapshot.get("postedDate").toString();
+                        String collabStatus = documentSnapshot.get("collabStatus").toString();
+                        String projectDesc = documentSnapshot.get("projectDesc").toString();
+                        String projectVisible = documentSnapshot.get("projectVisible").toString();
+                        String numberApplicants = documentSnapshot.get("numApplicants").toString();
+                        String numberSelectedApplicants = documentSnapshot.get("numSelectedApplicants").toString();
+                        myCollabs.add(new MyCollabsFour(posterTitle, projectTitle, projectDesc, postedDate, collabStatus, projectVisible, numberApplicants, numberSelectedApplicants));
 
+                        myCollabs4LayoutManager = new LinearLayoutManager(getContext());
+                        myCollabs4Adapter = new MyCollabsFourAdapter(myCollabs);
+                        collab4RecyclerView.setHasFixedSize(true);
+                        collab4RecyclerView.setLayoutManager(myCollabs4LayoutManager);
+                        collab4RecyclerView.setAdapter(myCollabs4Adapter);
+                    }
+                } else {
+                    collab4RecyclerView.setVisibility(View.GONE);
+                    swipeDownRefreshTV.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
