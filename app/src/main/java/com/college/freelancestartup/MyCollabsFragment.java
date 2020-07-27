b3f651af06610586131d69f2eb88ba66f120dae8
Yash Umale <yashumale223@gmail.com>
2020-07-27 15:25:25 +0530

    Fourth collab ViewPager setting up

--- app/src/main/java/com/college/freelancestartup/MyCollabsFragment.java
+++ app/src/main/java/com/college/freelancestartup/MyCollabsFragment.java
@@ -4,18 +4,50 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 class MyCollabsFragment extends Fragment {
 
     private View root;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView collab4RecyclerView;
+    private TextView swipeDownRefreshTV, emptyTV;
+    private FirebaseFirestore db;
+    private FirebaseAuth firebaseAuth;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        root = inflater.inflate(R.layout.)
+        root = inflater.inflate(R.layout.student_collab_mycollabs_frag, container, false);
+        db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        swipeDownRefreshTV = root.findViewById(R.id.swipeRefreshTVCollab4);
+        swipeRefreshLayout = root.findViewById(R.id.swipeRefreshLayoutCollab4);
+        emptyTV = root.findViewById(R.id.log_collab4_emptyTV);
+        collab4RecyclerView = root.findViewById(R.id.mycollabs_collabs4_recyclerView);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                showCollab4();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+        return root;
+    }
+
+    private void showCollab4(){
+
     }
 }
