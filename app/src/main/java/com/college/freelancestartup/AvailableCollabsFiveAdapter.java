0a9050cd8bc3ba1bc19c36bed2b1596dd09468f0
Yash Umale <yashumale223@gmail.com>
2020-07-28 19:46:04 +0530

    Fifth Collab Fragment Setup (12)

--- app/src/main/java/com/college/freelancestartup/AvailableCollabsFiveAdapter.java
+++ app/src/main/java/com/college/freelancestartup/AvailableCollabsFiveAdapter.java
@@ -1,5 +1,14 @@
 package com.college.freelancestartup;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -11,4 +20,33 @@ class AvailableCollabsFiveAdapter extends RecyclerView.Adapter<AvailableCollabsF
     public AvailableCollabsFiveAdapter(ArrayList<AvailableCollabsFive> availableCollabsExampleList){
         AvailableCollabsList = availableCollabsExampleList;
     }
+
+    @NonNull
+    @Override
+    public AvailableCollabsFiveAdapter.AvailableCollabsFiveViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.find_collab5_cardview, parent, false);
+        AvailableCollabsFiveAdapter.AvailableCollabsFiveViewHolder availableCollabsFiveViewHolder = new AvailableCollabsFiveAdapter.AvailableCollabsFiveViewHolder(v);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AvailableCollabsFiveOpenActivity.class);
+                String transitionName = view.getResources().getString(R.string.transitionAnimation);
+                View viewStart = view.findViewById(R.id.find_collab5_cardview);
+
+                intent.putExtra(name, "name");
+                intent.putExtra(department, "department");
+                intent.putExtra(skills, "skills");
+                intent.putExtra(semester, "semester");
+                intent.putExtra(noCollabs, "numberCollabs");
+                intent.putExtra(noProjects, "numberProjects");
+
+                ActivityOptionsCompat options =
+                        ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) view.getContext(), viewStart, transitionName);
+
+                ActivityCompat.startActivity(view.getContext(), intent, options.toBundle());
+            }
+        });
+        return allColabsOneViewHolder;
+    }
+
 }
