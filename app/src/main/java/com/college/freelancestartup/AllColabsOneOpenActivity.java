17cd54b33db6373989ca6cac2c64a27cba90a1cc
Yash Umale <yashumale223@gmail.com>
2020-07-30 16:58:06 +0530

    Adding backend to buttons involved (2)

--- app/src/main/java/com/college/freelancestartup/AllColabsOneOpenActivity.java
+++ app/src/main/java/com/college/freelancestartup/AllColabsOneOpenActivity.java
@@ -1,15 +1,20 @@
 package com.college.freelancestartup;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 class AllColabsOneOpenActivity extends AppCompatActivity {
 
     private TextView posterName1, projectTitle1, postedDate1, projectDesc1, openFor1, skills1;
-    private
+    private MaterialButton messageButton, applyButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +27,8 @@ class AllColabsOneOpenActivity extends AppCompatActivity {
         projectDesc1 = findViewById(R.id.collab1_projectDesc);
         openFor1 = findViewById(R.id.collab1_openFor);
         skills1 = findViewById(R.id.collab1_skills);
+        applyButton = findViewById(R.id.collab1_applyBtn);
+        messageButton = findViewById(R.id.collab1_messageBtn);
 
         posterName1.setText(getIntent().getExtras().get("posterName1").toString());
         projectTitle1.setText(getIntent().getExtras().get("projectTitle1").toString());
@@ -29,5 +36,16 @@ class AllColabsOneOpenActivity extends AppCompatActivity {
         projectDesc1.setText(getIntent().getExtras().get("projectDesc1").toString());
         openFor1.setText(getIntent().getExtras().get("openFor1").toString());
         skills1.setText(getIntent().getExtras().get("skills1").toString());
+
+        applyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog applyCollab1 = new MaterialAlertDialogBuilder(AllColabsOneOpenActivity.this)
+                        .setView()
+                        .setTitle("Apply for collaboration")
+                        .setMessage("Write a brief description about yourself and how you'd be contributing to the project.")
+                        .
+            }
+        });
     }
 }
