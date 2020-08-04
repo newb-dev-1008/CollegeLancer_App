9b048181e1619850668a55017043cc1b9cf35293
Yash Umale <yashumale223@gmail.com>
2020-08-04 20:21:30 +0530

    Adding activity to upload project for collab (10)

--- app/src/main/java/com/college/freelancestartup/StudentAddProjectForCollab.java
+++ app/src/main/java/com/college/freelancestartup/StudentAddProjectForCollab.java
@@ -1,21 +1,27 @@
 package com.college.freelancestartup;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
+import java.util.Random;
 
 public class StudentAddProjectForCollab extends AppCompatActivity {
 
@@ -26,6 +32,7 @@ public class StudentAddProjectForCollab extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseFirestore db;
     int numberPicked, numberApps, endVotes;
+    MaterialButton addExistingButton, addNewButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,6 +45,8 @@ public class StudentAddProjectForCollab extends AppCompatActivity {
         projectOpenFor = findViewById(R.id.projectOpenForEditText);
         projectDesc = findViewById(R.id.projDescEditText);
         projectSkills = findViewById(R.id.proj_Skills);
+        addExistingButton = findViewById(R.id.addExistingProjectButton);
+        addNewButton = findViewById(R.id.addNewProjectButton);
         numberApps = 0;
         numberPicked = 0;
         endVotes = 0;
@@ -57,6 +66,44 @@ public class StudentAddProjectForCollab extends AppCompatActivity {
             }
         });
 
+        addExistingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // finish this
+            }
+        });
+
+        addNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNewProjectFunction();
+            }
+        });
 
     }
+
+    private void addNewProjectFunction() {
+        AlertDialog.Builder addProjectDialog = new MaterialAlertDialogBuilder(this)
+                .setTitle("Add project?")
+                .setMessage("Your project will be out in the open for developers to see and apply for collaboration.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        postDate = calendar.getTime().toString();
+                        projectStatus = "Open";
+                    }
+                })
+    }
+
+    private String generateProjectID() {
+        StringBuilder sb = new StringBuilder(30);
+        String DATA = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random RANDOM = new Random();
+        for (int i = 0; i < 30; i++) {
+            sb.append(DATA.charAt(RANDOM.nextInt(DATA.length())));
+        }
+
+        return sb.toString();
+    }
+
 }
