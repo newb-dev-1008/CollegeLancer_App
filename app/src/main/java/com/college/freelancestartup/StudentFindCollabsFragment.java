d2dea2b993acf4a1e5fd379dc464bdaa22b74405
Yash Umale <yashumale223@gmail.com>
2020-08-04 14:57:01 +0530

    Debug and fix collab activity (1)

--- app/src/main/java/com/college/freelancestartup/StudentFindCollabsFragment.java
+++ app/src/main/java/com/college/freelancestartup/StudentFindCollabsFragment.java
@@ -8,12 +8,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class StudentFindCollabsFragment extends Fragment {
+
+    private View root;
+    private ViewPager complaintViewPager;
+    private TabLayout complaintTabLayout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.student_find_collabs_frag, container, false);
+        return inflater.inflate(R.layout.collab_viewpager, container, false);
     }
 }
