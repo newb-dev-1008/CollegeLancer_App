8d5f03ea298c57e06555b7f8857ba3773094663e
Yash Umale <yashumale223@gmail.com>
2020-08-06 16:09:10 +0530

    Report Bug Fragment (1) [Activity]

--- app/src/main/java/com/college/freelancestartup/ReportBugFragment.java
+++ app/src/main/java/com/college/freelancestartup/ReportBugFragment.java
@@ -4,16 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+
 public class ReportBugFragment extends Fragment {
+
+    private EditText bugLocationET, bugDescriptionET;
+    private RadioGroup bugFrequencyRadioGroup, bugStallRadioGroup;
+    private View root;
+    private MaterialButton addScreenshots, reportBug;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.report_bug_frag, container, false);
+        root = inflater.inflate(R.layout.report_bug_frag, container, false);
+        bugLocationET = root.findViewById(R.id.bugLocationEditText);
+        bugDescriptionET = root
+
+        return root;
     }
 }
