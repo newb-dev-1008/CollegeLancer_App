8a39eafc7d277cc2f407c44cfb8fbf21974e5b66
Yash Umale <yashumale223@gmail.com>
2020-08-18 22:21:22 +0530

    Working on ApplicantLogAdapter (3)

--- app/src/main/java/com/college/freelancestartup/ApplicantLogAdapter.java
+++ app/src/main/java/com/college/freelancestartup/ApplicantLogAdapter.java
@@ -1,7 +1,11 @@
 package com.college.freelancestartup;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -9,10 +13,17 @@ import java.util.ArrayList;
 class ApplicantLogAdapter extends RecyclerView.Adapter<ApplicantLogAdapter.ApplicantLogViewHolder> {
 
     private ArrayList<AvailableCollabsFive> ApplicantLogList;
-    private String name, department, userEmail, projectID, pickedStatusT;
+    private String name, semester, userEmail, projectID, pickedStatusT;
     private Context context;
 
     public ApplicantLogAdapter(ArrayList<AvailableCollabsFive> applicantLogExampleList){
         ApplicantLogList = applicantLogExampleList;
     }
+
+    @NonNull
+    @Override
+    public ApplicantLogAdapter.ApplicantLogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.find_collab5_cardview, parent, false);
+        context = v.getContext();
+        ApplicantLogAdapter.ApplicantLogViewHolder applicantLogViewHolder = new ApplicantLogAdapter.ApplicantLogViewHolder(v);
 }
