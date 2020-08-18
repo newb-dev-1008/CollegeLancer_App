9de95ccea3614ac828ba0a7653cdfe086be3054e
Yash Umale <yashumale223@gmail.com>
2020-08-18 21:31:30 +0530

    Changes to ApplicantLogOpenActivity (1)

--- app/src/main/java/com/college/freelancestartup/ApplicantLogOpenActivity.java
+++ app/src/main/java/com/college/freelancestartup/ApplicantLogOpenActivity.java
@@ -1,8 +1,15 @@
 package com.college.freelancestartup;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 class ApplicantLogOpenActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.applicant_log_opencard);
+    }
 }
