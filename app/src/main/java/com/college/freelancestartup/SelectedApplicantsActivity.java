b4076260be8e470a1fe8204cc08a6ba6234f15db
Yash Umale <yashumale223@gmail.com>
2020-08-20 22:21:55 +0530

    Adding selected applicants button (12)

--- app/src/main/java/com/college/freelancestartup/SelectedApplicantsActivity.java
+++ app/src/main/java/com/college/freelancestartup/SelectedApplicantsActivity.java
@@ -1,10 +1,12 @@
 package com.college.freelancestartup;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -13,6 +15,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class SelectedApplicantsActivity extends AppCompatActivity {
 
@@ -56,8 +60,28 @@ public class SelectedApplicantsActivity extends AppCompatActivity {
                 .whereEqualTo("picked", "Selected").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                if (queryDocumentSnapshots.size() > 0){
+                    emptyTV.setVisibility(View.GONE);
+                    ArrayList<MyCollabsFour> myCollabs = new ArrayList<>();
+                    for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                        String name = documentSnapshot.get("name").toString();
+                        String semester = documentSnapshot.get("studentSemester").toString();
+                        String skills = documentSnapshot.get("studentSkills").toString();
+                        String department = documentSnapshot.get("department").toString();
+                        String previousCollabs = documentSnapshot.get("numberCollabs").toString();
+                        String previousProjects = documentSnapshot.get("numberProjects").toString();
+                        myCollabs.add(new MyCollabsFour(posterTitle, projectTitle, projectDesc, postedDate, collabStatus, projectVisible, numberApplicants, numberSelectedApplicants, projectSkills, projectOpenFor, projectID));
 
+                        myCollabs4LayoutManager = new LinearLayoutManager(SelectedApplicantsActivity.this);
+                        myCollabs4Adapter = new MyCollabFourAdapter(myCollabs);
+                        collab4RecyclerView.setHasFixedSize(true);
+                        collab4RecyclerView.setLayoutManager(myCollabs4LayoutManager);
+                        collab4RecyclerView.setAdapter(myCollabs4Adapter);
+                    }
+                } else {
+                    collab4RecyclerView.setVisibility(View.GONE);
+                    swipeDownRefreshTV.setVisibility(View.GONE);
+                }
                 }
             }
         })
