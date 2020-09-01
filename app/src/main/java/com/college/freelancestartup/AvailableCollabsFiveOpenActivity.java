bbdee6927b3d5bd2ce0066d1d0df5fae8b0093be
Yash Umale <yashumale223@gmail.com>
2020-09-01 22:22:09 +0530

    Implementing AsyncTask in Fifth Collab (3) [ProgressBar added to XML]

--- app/src/main/java/com/college/freelancestartup/AvailableCollabsFiveOpenActivity.java
+++ app/src/main/java/com/college/freelancestartup/AvailableCollabsFiveOpenActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
